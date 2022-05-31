3a2e3b037c399a0283bc2a3d9d8800752497a243
Edward <edwardrockrw@gmail.com>
2022-05-31 17:30:38 +0800

    setup code-base

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -14,5 +14,24 @@ export default class PostsController {
         return this.postsService.getAllPosts();
     }
     
+    @Get(':id')
+    getPostById(@Param('id') id:string){
+        return this.postsService.getPostById(Number(id));
+    }
+    
+    @Post()
+    async createPost(@Body() post: CreatePostDto){
+        return this.postsService.createPost(post);
+    }
+    
+    @Put(':id')
+    async replacePost(@Param('id') id:string , @Body() post:UpdatePostDto){
+        return this.postsService.replacePost(Number(id) , post);
+    }
+    
+    @Delete(':id')
+    async deletePost(@Param('id') id:string){
+        return this.postsService.deletePost(Number(id));
+    }
 }
 
