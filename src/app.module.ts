0b3962946613d3865f1cb64ab0fcd5996f9aaf7d
Edward <edwardrockrw@gmail.com>
2022-06-13 15:15:49 +0800

    add jwt & authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,19 +4,23 @@ import { Module } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
 import {ConfigModule} from "@nestjs/config";
 import * as Joi from "@hapi/joi";
-import {DatabaseModule} from "./posts/database.module";
+import {DatabaseModule} from "./database/database.module";
+import {AuthenticationModule} from "./authentication/authentication.module";
+import {UsersModule} from "./users/users.module";
 
 @Module({
   imports: [
-      PostsModule ,  
+      PostsModule , AuthenticationModule,UsersModule, 
       ConfigModule.forRoot({
         validationSchema: Joi.object({
-          POSTGRES_HOST: Joi.string().required(),
-          POSTGRES_PORT: Joi.number().required(),
-          POSTGRES_USER: Joi.string().required(),
-          POSTGRES_PASSWORD: Joi.string().required(),
-          POSTGRES_DB: Joi.string().required(),
-          PORT: Joi.number(),
+            POSTGRES_HOST: Joi.string().required(),
+            POSTGRES_PORT: Joi.number().required(),
+            POSTGRES_USER: Joi.string().required(),
+            POSTGRES_PASSWORD: Joi.string().required(),
+            POSTGRES_DB: Joi.string().required(),
+            PORT: Joi.number(), 
+            JWT_SECRET: Joi.string().required(),
+            JWT_EXPIRATION_TIME: Joi.string().required(),
         })
       }),
       DatabaseModule,
